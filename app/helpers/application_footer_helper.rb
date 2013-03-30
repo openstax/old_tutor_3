dd4b02bc722c0d503261b37fdaed2600b0620b0c
Kevin Burleigh <klb@kindling>
2013-03-30 01:13:48 -0700

    LegalHelper ==> ApplicationFooterHelper

--- app/helpers/application_footer_helper.rb
+++ app/helpers/application_footer_helper.rb
@@ -1,7 +1,7 @@
 # Copyright 2011-2013 Rice University. Licensed under the Affero General Public 
 # License version 3 or later.  See the COPYRIGHT file for details.
 
-module LegalHelper
+module ApplicationFooterHelper
 
   def copyright_text
     year_range = "2011-#{Time.now.year}".sub(/\A(\d+)-\1\z/, '\1');
