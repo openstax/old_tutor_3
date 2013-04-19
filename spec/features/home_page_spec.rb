ba4975b90161a78834745ace8a02b8903b13e66f
Kevin Burleigh <klb@kindling>
2013-04-18 17:26:53 -0700

    tweaked element data-test attribute names

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -4,17 +4,17 @@ describe "home page" do
 
   it "appears when visiting the root path" do
     visit root_path
-    page.should have_css('[data-test-page="home page"]')
+    page.should have_css('[data-test-page-home]')
   end
 
   it "contains a link to join" do
     visit root_path
-    page.has_link?('[data-test-link="join"]')
+    page.has_link?('[data-test-link-join]')
   end
 
   it "contains a link to sign in" do
     visit root_path
-    page.has_link?('[data-test-link="sign in"]')
+    page.has_link?('[data-test-link-sign-in]')
   end
 
 end
