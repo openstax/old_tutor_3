af655b7e64be84353f0c479cffb9fc3ac8895d7e
Kevin Burleigh <klb@kindling>
2013-05-06 17:14:23 -0700

    refactored top_nav_render to DRY up code

--- app/helpers/application_top_nav_helper.rb
+++ app/helpers/application_top_nav_helper.rb
@@ -3,58 +3,36 @@
 
 module ApplicationTopNavHelper
 
+  ABOUT_TEXT           = "ABOUT"
+  DASHBOARD_TEXT       = "DASHBOARD"
+  COURSE_CATALOG_TEXT  = "COURSE CATALOG"
+  CURRENT_CLASSES_TEXT = "CURRENT CLASSES"
+  MY_TUTOR_TEXT        = "MY TUTOR"
+  HELP_TEXT            = "HELP"
+
   def top_nav_about_link
-    render  :partial  => "layouts/application_top_nav_link", 
-            :locals   => {  :target_text            => "ABOUT",
-                            :target_image           => "",
-                            :target_id              => "top-nav-about-link",
-                            :target_path            => "#",
-                            :target_current_symbol  => :top_nav_about_current }
+    top_nav_render ABOUT_TEXT, target_path: "#"
   end
 
   def top_nav_dashboard_link
-    render  :partial  => "layouts/application_top_nav_link", 
-            :locals   => {  :target_text            => "DASHBOARD",
-                            :target_image           => "",
-                            :target_id              => "top-nav-dashboard-link",
-                            :target_path            => dashboard_page_path,
-                            :target_current_symbol  => :top_nav_dashboard_current }
+    top_nav_render DASHBOARD_TEXT
   end
 
   def top_nav_course_catalog_link
-    render  :partial  => "layouts/application_top_nav_link", 
-            :locals   => {  :target_text            => "COURSE CATALOG",
-                            :target_image           => "",
-                            :target_id              => "top-nav-course-catalog-link",
-                            :target_path            => course_catalog_page_path,
-                            :target_current_symbol  => :top_nav_course_catalog_current }
+    top_nav_render COURSE_CATALOG_TEXT
   end
 
   def top_nav_current_classes_link
-    render  :partial  => "layouts/application_top_nav_link", 
-            :locals   => {  :target_text            => "CURRENT CLASSES",
-                            :target_image           => "",
-                            :target_id              => "top-nav-current-classes-link",
-                            :target_path            => "#",
-                            :target_current_symbol  => :top_nav_current_classes_current }
+    top_nav_render CURRENT_CLASSES_TEXT, target_path: "#"
   end
 
   def top_nav_my_tutor_link
-    render  :partial  => "layouts/application_top_nav_link", 
-            :locals   => {  :target_text            => "MY TUTOR",
-                            :target_image           => "",
-                            :target_id              => "top-nav-my-tutor-link",
-                            :target_path            => "#",
-                            :target_current_symbol  => :top_nav_my_tutor_current }
+    top_nav_render MY_TUTOR_TEXT, target_path: "#"
   end
 
   def top_nav_help_link
-    render  :partial  => "layouts/application_top_nav_link", 
-            :locals   => {  :target_text            => "HELP",
-                            :target_image           => image_tag('help_icon_v4.png', :class => "help-link-icon"),
-                            :target_id              => "top-nav-help-link",
-                            :target_path            => "#",
-                            :target_current_symbol  => :top_nav_help_current }
+    top_nav_render HELP_TEXT, target_path:  "#",
+                              target_image: image_tag('help_icon_v4.png', class: 'help-link-icon')
   end
 
   def top_nav_about_active
@@ -93,13 +71,33 @@ module ApplicationTopNavHelper
     end
   end
 
-  def top_nav_current_link_class
-    "current "
-  end
-
   def top_nav_home_page_background
     content_for :top_nav_class do
       "home-page "
     end
   end
+
+protected
+
+  def top_nav_render(text, options={})
+    partial = "layouts/application_top_nav_link"
+
+    target_text           = text
+    target_image          = options.fetch(:target_image, "")
+    target_id             = "top-nav-#{text.downcase.tr(' ', '-')}-link"
+    target_path           = options.fetch(:target_path) { send("#{text.downcase.tr(' ', '_')}_page_path") }
+    target_current_symbol = "top_nav_#{text.downcase.tr(' ', '_')}_current".to_sym
+
+    render partial: partial,
+           locals:  { target_text:           target_text,
+                      target_image:          target_image,
+                      target_id:             target_id,
+                      target_path:           target_path,
+                      target_current_symbol: target_current_symbol }
+  end
+
+  def top_nav_current_link_class
+    "current "
+  end
+
 end
